2298098266e00752e9c5a27085371c37ebb05914
Sergei Alimov <8912652@gmail.com>
2018-12-01 22:38:56 +0300

    add expand button

--- src/components/topPane.jsx
+++ src/components/topPane.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './../styles/topPane.css';
+import expandImg from './../img/expand.png';
 
 class TopPane extends Component {
   render() {
     return (
       <div class="topPane">
         <div class="editor">
-        <div class="topBar"></div>
+        <div class="topBar">
+          <p class="title">Editor</p>
+          <input type="image" src={expandImg} width="20" height="20" alt="Expand"></input>
+        </div>
       </div>
     </div>
     )
