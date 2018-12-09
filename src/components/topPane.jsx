08a545e2b8b8a68f594149c2d9320a94c6bfd368
Sergei Alimov <8912652@gmail.com>
2018-12-09 21:51:00 +0300

    enable onChange back

--- src/components/topPane.jsx
+++ src/components/topPane.jsx
@@ -3,31 +3,29 @@ import { connect } from 'react-redux';
 import './../styles/topPane.css';
 import expandImg from './../img/expand.png';
 
-const TopPane = ({text}) => {
-  return (
-    <div className="topPane">
-      <div className="editor">
-        <div className="topBar">
-          <p className="title">Editor</p>
-          <input type="image" src={expandImg} width="20" height="20" alt="Expand"/>
-        </div>
-      <textarea id="editor">{text}</textarea>
-      {/*<textarea id="editor" onChange={this.onChange}>{this.props.text}</textarea>*/}
+class TopPane extends Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(event) {
+    this.props.translate(event.target.value);
+  }
+  render () {
+    return (
+      <div className="topPane">
+        <div className="editor">
+          <div className="topBar">
+            <p className="title">Editor</p>
+            <input type="image" src={expandImg} width="20" height="20" alt="Expand"/>
+          </div>
+        <textarea id="editor" onChange={this.onChange}>{this.props.text}</textarea>
+      </div>
     </div>
-  </div>
-  )
+    )
+  }
 }
-
-// class TopPane extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.onChange = this.onChange.bind(this)
-//   }
-// 
-//   onChange(event) {
-//     this.props.translate(event.target.value);
-//   }
-
 
 const mapStateToProps = state => {
   const { text } = state;
