f4cf8a9b9b6b61d4513dd56aeb64f7185eddb1a7
Sergei Alimov <8912652@gmail.com>
2018-12-09 21:21:53 +0300

    remove unnesessary variable

--- src/store.js
+++ src/store.js
@@ -2,6 +2,5 @@ import { reducer } from './reducers/reducer.js';
 import { createStore } from 'redux';
 
 const extenstion = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(reducer, extenstion);
 
-export default store;
+export default createStore(reducer, extenstion);;
