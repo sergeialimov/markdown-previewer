1ccfef781e69df11ec2606a781969232c34fc08a
Sergei Alimov <8912652@gmail.com>
2018-12-11 12:34:48 +0300

    add autotests

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,6 +4,14 @@ import BottomPane from './../components/bottomPane.jsx';
 import './../styles/app.css';
 
 class App extends Component {
+  // autotests for FreeCodeCamp
+  componentDidMount () {
+    const script = document.createElement("script");
+    script.src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
   render() {
     return (
       <div className="app">
