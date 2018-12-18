f72541dadc8244477e6fc1195eeecaa305b7786a
Sergei Alimov <8912652@gmail.com>
2018-12-18 12:20:14 +0300

    remove semicolon

--- src/store.js
+++ src/store.js
@@ -3,4 +3,4 @@ import { createStore } from 'redux';
 
 const extenstion = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-export default createStore(reducer, extenstion);;
+export default createStore(reducer, extenstion);
